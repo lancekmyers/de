6a1f528d475cbb693922c8481ec942a842c0d77f
lancekmyers <lancekmyers@gmail.com>
2023-05-20 12:08:41 -0500

    add rkf45 method

--- src/Solver/ButcherTableau.hs
+++ src/Solver/ButcherTableau.hs
@@ -1,7 +1,12 @@
+{-# LANGUAGE UndecidableInstances #-}
+
 module Solver.ButcherTableau where
 
 import Control.Monad.State
+import Data.Data (Proxy)
+import Data.Ratio
 import Linear
+import Solver.Class
 import Term
 
 newtype BT a = BT ([a], [(a, [a])])
@@ -26,16 +31,30 @@ butcherTableau coeffs ode y0 t0 h = foldr go [] coeffs
         u = control ode (t0, t0 + h)
         k = prod ode v u
 
-rk45 :: Floating a => BT a
-rk45 =
-  BT
-    ( [1 / 6, 1 / 3, 1 / 3, 1 / 6],
-      [ (0, []),
-        (0.5, [0.5]),
-        (0.5, [0, 0.5]),
-        (1.0, [0, 0, 1])
-      ]
-    )
+rkf45 :: (Floating a) => ([a], [a], [(a, [a])])
+rkf45 =
+  ( [25 / 216, 0, 1408 / 2565, 2197 / 1404, -1 / 5, 0],
+    [16 / 135, 0, 6656 / 12825, 28561 / 56430, -9 / 50, 2 / 55],
+    [ (0, []),
+      (0.25, [0.25]),
+      (3 / 8, [3 / 32, 9 / 32]),
+      (12 / 13, [1932 / 2197, -7200 / 2197, 7296 / 2197]),
+      (1, [439 / 216, -8, 3680 / 513, -845 / 4104]),
+      (1, [8 / 17, 2, -3544 / 2565, 1859 / 4104, -11 / 40])
+    ]
+  )
+
+data RKF45 v a = RKF45 a -- Error estimate
+
+instance (Term ode, Metric (T ode)) => Solver RKF45 ode where
+  initSolver _ _ _ = RKF45 0
+  step ode y0 (t0, t1) = do
+    let (final1, final2, coeffs) = rkf45
+    let ks = butcherTableau coeffs ode y0 t0 (t1 - t0)
+    let y1 = foldr (^+^) y0 $ zipWith (*^) final1 ks
+    let y1' = foldr (^+^) y0 $ zipWith (*^) final2 ks
+    put $ RKF45 (norm $ y1 ^-^ y1')
+    return y1
 
 stepButcher ::
   (Term de, Floating a, MonadState (sol (T de) a) m) =>
