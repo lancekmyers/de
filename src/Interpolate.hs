aa194847a6a23cd93c31456558404b912dc2d444
lancekmyers <lancekmyers@gmail.com>
2023-09-28 23:45:21 -0500

    add plain polynomial interpolant

--- src/Interpolate.hs
+++ src/Interpolate.hs
@@ -25,6 +25,7 @@ data Interp de a
       (S de a)
       (S de a)
   | Lin (a, a) (T de a) (T de a)
+  | Poly (a, a) (Vector (T de a))
 
 interp ::
   forall de a.
@@ -33,6 +34,9 @@ interp ::
   a ->
   Interp de a ->
   T de a
+interp de c (Poly (t0, t1) coeffs) = polyEval coeffs c'
+  where
+    c' = (c - t0) / (t1 - t0)
 interp de x (Lin (x0, x1) p p') =
   t *^ p ^+^ (1 - t) *^ p'
   where
@@ -64,16 +68,20 @@ interp de t (H4 (t0, t1) ymid y0 y1 f0 f1) = polyEval coeffs t'
     -- reversing [y0, one f0, _c, _b, _a]
     coeffs = V.fromList [_a, _b, _c, one f0, y0]
 
-rightMost :: Interp de a -> T de a
+rightMost :: (Num a, Additive (T de)) => Interp de a -> T de a
+rightMost (Poly _ coeffs) = polyEval coeffs 1
 rightMost (Lin _ _ y1) = y1
 rightMost (H3 _ _ _ _ y1) = y1
 rightMost (H4 _ _ _ y1 _ _) = y1
 
+leftMost :: (Num a, Additive (T de)) => Interp de a -> T de a
+leftMost (Poly _ coeffs) = polyEval coeffs 0
 leftMost (Lin _ y0 _) = y0
 leftMost (H3 _ _ y0 _ _) = y0
 leftMost (H4 _ _ y0 _ _ _) = y0
 
 timeInterval :: Interp de a -> (a, a)
+timeInterval (Poly interval _) = interval
 timeInterval (H4 interval _ _ _ _ _) = interval
 timeInterval (H3 interval _ _ _ _) = interval
 timeInterval (Lin interval _ _) = interval
@@ -102,7 +110,7 @@ evalSol de (t : ts) (i : is)
   | contains i t = interp de t i : evalSol de ts is
   | otherwise = evalSol de (t : ts) is
 
-knots :: [Interp de a] -> [(a, T de a)]
+knots :: (Num a, Term de) => [Interp de a] -> [(a, T de a)]
 knots [] = []
 knots (i : is) =
   (fst $ timeInterval i, leftMost i) :
